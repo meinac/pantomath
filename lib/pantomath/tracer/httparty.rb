1a7e9d008b0f1ff8ffef5b9e8b6ffe6fc87f33e9
Mehmet Emin INAC <mehmetemininac@gmail.com>
2018-11-27 11:49:08 +0300

    Implement HTTParty tracer

---
+++ lib/pantomath/tracer/httparty.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# Just inlcude this file to enable this tracer.
+module Pantomath
+  module Tracer
+    module HTTParty
+
+      def perform(&block)
+        start_span
+        super
+      ensure
+        close_span
+      end
+
+      def start_span
+        Pantomath.tracer.start_active_span(
+          span_name,
+          tags: {
+            "span.kind" => "external/http",
+            "span.tracer" => "Pantomath::Tracer::HTTParty",
+            "external.http.request.method" => http_method::METHOD,
+            "external.http.request.host" => uri.host,
+            "external.http.request.path" => uri.path
+          }
+        )
+      end
+
+      def close_span
+        if Pantomath.active_scope
+          Pantomath.active_span.set_tag("external.http.request.response_code", last_response.code.to_i)
+          Pantomath.active_scope.close
+        end
+      end
+
+      def span_name
+        "NET/HTTP Call to #{uri.host}"
+      end
+
+    end
+  end
+end
+
+HTTParty::Request.send(:prepend, Pantomath::Tracer::HTTParty)
